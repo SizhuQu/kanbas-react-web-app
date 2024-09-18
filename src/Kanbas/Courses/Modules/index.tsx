7e702956879cf1087bf654baf1d15d6eeea750cf
Sizhu Qu <144123630+SizhuQu@users.noreply.github.com>
2024-09-18 16:14:07 -0400

    changes

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -1,29 +1,53 @@
+import { useState } from "react";
 export default function Modules() {
+    const [isCollapsed, isAlreadyCollapsed] = useState(false);
+    const expandCollapse = () => { isAlreadyCollapsed(!isCollapsed); };
+    const collapse = isCollapsed ? "collapsed" : "";
+
     return (
       <div>
-        {/* Implement Collapse All button, View Progress button, etc. */}
+        <div>
+            <button onClick={expandCollapse}>Collapse All</button>
+            <button>
+                View Progress
+            </button>
+            <select>
+                <option>Publish All</option>
+                <option>Publish Module 1</option>
+                <option>Publish Module 2</option>
+                <option>Publish Module 3</option>
+            </select>
+            <button>+ Module</button>
+           
+        </div>
         <ul id="wd-modules">
-          <li className="wd-module">
+            <li className="wd-module">
             <div className="wd-title">Week 1</div>
-            <ul className="wd-lessons">
-              <li className="wd-lesson">
-                <span className="wd-title">LEARNING OBJECTIVES</span>
-                <ul className="wd-content">
-                  <li className="wd-content-item">Introduction to the course</li>
-                  <li className="wd-content-item">Learn what is Web Development</li>
+            {!isCollapsed && (
+                <ul className="wd-lessons">
+                <li className="wd-lesson">
+                    <span className="wd-title">LEARNING OBJECTIVES</span>
+                    <ul className="wd-content">
+                    <li className="wd-content-item">Introduction to the course</li>
+                    <li className="wd-content-item">Learn what is Web Development</li>
+                    </ul>
+                </li>
                 </ul>
-              </li>
-            </ul>
-          </li>
-          <li className="wd-module">
-            <div className="wd-title">Week 2</div>
+            )}
+            </li>
+            
+            <li className="wd-module">
+          <div className="wd-title">Week 2</div>
+          {!isCollapsed && (
             <ul className="wd-lessons">
               <li className="wd-lesson">
                 <span className="wd-title">LEARNING OBJECTIVES</span>
               </li>
             </ul>
-          </li>
-        </ul>
-      </div>
-  );}
+          )}
+        </li>
+      </ul>
+    </div>
+  );
+}
   
