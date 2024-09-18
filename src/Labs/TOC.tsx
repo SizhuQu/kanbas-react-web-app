9d0b9a7b37501600f11902fe956daaf4b5be927c
Sizhu Qu <144123630+SizhuQu@users.noreply.github.com>
2024-09-18 14:54:13 -0400

    changes

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function TOC() {
     return (
       <ul>
@@ -5,6 +7,7 @@ export default function TOC() {
         <li><Link to="/Labs/Lab1">Lab 1</Link></li>
         <li><Link to="/Labs/Lab2">Lab 2</Link></li>
         <li><Link to="/Labs/Lab3">Lab 3</Link></li>
+        <li><Link to="/Kanbas">Kanbas</Link></li>
       </ul>
     );
  }  
