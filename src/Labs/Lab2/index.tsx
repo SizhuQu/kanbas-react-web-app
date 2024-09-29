8cc2f797017aa62f65c3765d31b3217eabc15615
Sizhu Qu <144123630+SizhuQu@users.noreply.github.com>
2024-09-29 03:16:15 +0100

    3.2

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -11,6 +11,7 @@ import Zindex from "./Zindex";
 import Float from "./Float";
 import GridLayout from "./GridLayout";
 import Flex from "./Flex";
+import ReactIconsSampler from "./ReactIcons";
 
 export default function Lab2() {
   return (
@@ -81,6 +82,7 @@ export default function Lab2() {
       <Float />
       <GridLayout />
       <Flex />
+      <ReactIconsSampler />
 
     </div>
   );
