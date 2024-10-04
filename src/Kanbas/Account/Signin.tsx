158a299b79cd4251c334fb5b964956e1b707813c
Sizhu Qu <144123630+SizhuQu@users.noreply.github.com>
2024-10-03 23:17:36 -0400

    4.9

--- src/Kanbas/Account/Signin.tsx
+++ src/Kanbas/Account/Signin.tsx
@@ -8,25 +8,25 @@ export default function Signin() {
         <div className="col-md-4 offset-md-4">
           <h1 className="text-center">Sign in</h1>
           <input
-            id="wd-username"
-            placeholder="username"
+            id="signin-username"
+            placeholder="Username"
             className="form-control mb-3"
           />
           <input
-            id="wd-password"
-            placeholder="password"
+            id="signin-password"
+            placeholder="Password"
             type="password"
             className="form-control mb-3"
           />
           <Link
-            id="wd-signin-btn"
-            to="/Kanbas/Account/Profile"
+            id="signin-btn"
+            to="Profile" 
             className="btn btn-primary w-100 mb-3"
           >
             Sign in
           </Link>
           <div className="text-center">
-            <Link id="wd-signup-link" to="/Kanbas/Account/Signup">
+            <Link id="signup-link" to="Signup">
               Sign up
             </Link>
           </div>
