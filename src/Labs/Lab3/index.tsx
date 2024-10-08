fd91acaecc2c720def6c4d24f58b26eae179b951
Sizhu Qu <144123630+SizhuQu@users.noreply.github.com>
2024-10-08 09:55:47 -0400

    2.2.3

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,9 +1,16 @@
+import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
+import BooleanVariables from "./BooleanVariables";
+
 export default function Lab3() {
-    return (
-      <div id="wd-lab3">
+  return (
+    <div id="wd-lab3">
       <h3>Lab 3</h3>
+      <VariablesAndConstants />
+      <VariableTypes />
+      <BooleanVariables />
+
     </div>
-    );
-  }
-  
-  
+  );
+}
+
