793560f1766bba37acb8390285cf63379ec307ed
Sizhu Qu <144123630+SizhuQu@users.noreply.github.com>
2024-10-17 17:52:30 -0400

    3.6

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -6,17 +6,19 @@ import AssignmentEditor from "./Assignments/Editor";
 import { FaAlignJustify } from "react-icons/fa";
 import PeopleTable from "./People/Table";
 import { courses } from "../Database";
-import { Navigate, Route, Routes, useParams } from "react-router";
+import { Route, Routes, useParams, useLocation } from "react-router";
 
 export default function Courses() {
   const { cid } = useParams();
   const course = courses.find((course) => course._id === cid);
-
+  const { pathname } = useLocation();
+  
   return (
     <div id="wd-courses">
       <h2 className="text-danger">
         <FaAlignJustify className="me-4 fs-4 mb-1" />
-        {course && course.name}</h2>
+        {course && course.name} &gt; {pathname.split("/")[4]}
+      </h2>
       <hr />
       <div className="d-flex">
         <div className="d-none d-md-block">
