a218520a26ae370e2896bde9d4cb3b59bcf0aa87
Sizhu Qu <144123630+SizhuQu@users.noreply.github.com>
2024-10-17 14:03:14 -0400

    2.6

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -28,6 +28,10 @@ import DestructingImports from "./DestructingImports";
 import Classes from "./Classes";
 import Styles from "./Styles";
 import Add from "./Add";
+import Square from "./Square";
+import Highlight from "./Highlight";
+import AddPathParameters from "./AddPathParameters";
+import PathParameters from "./PathParameters";
 
 export default function Lab3() {
   return (
@@ -63,6 +67,17 @@ export default function Lab3() {
       <Classes />
       <Styles />
       <Add a={3} b={4} />
+      <h4>Square of 4</h4>
+      <Square>4</Square>
+
+      <Highlight>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
+        vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
+      </Highlight>
+      
+      <PathParameters  />
+      
+      <hr />
     </div>
   );
 }
--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -9,31 +9,16 @@ export default function TOC() {
           Labs
         </Link>
       </li>
-      <li className="nav-item">
-        <Link to="/Labs/Lab1" className={`nav-link ${pathname.includes("Lab1") ? "active" : ""}`}>
-          Lab 1
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link to="/Labs/Lab2" className={`nav-link ${pathname.includes("Lab2") ? "active" : ""}`}>
-          Lab 2
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link to="/Labs/Lab3" className={`nav-link ${pathname.includes("Lab3") ? "active" : ""}`}>
-          Lab 3
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link to="/Kanbas" className="nav-link">
-          Kanbas
-        </Link>
-      </li>
-      <li className="nav-item">
-        <a href="https://github.com/SizhuQu/kanbas-react-web-app.git" className="nav-link">
-          My GitHub
-        </a>
-      </li>
+      <li className="nav-item"><a id="wd-a1" href="#/Labs/Lab1"
+        className={`nav-link ${pathname.includes("Lab1") ? "active" : ""}`}>Lab 1</a></li>
+      <li className="nav-item"><a id="wd-a2" href="#/Labs/Lab2"
+        className={`nav-link ${pathname.includes("Lab2") ? "active" : ""}`}>Lab 2</a></li>
+      <li className="nav-item"><a id="wd-a3" href="#/Labs/Lab3"
+        className={`nav-link ${pathname.includes("Lab3") ? "active" : ""}`}>Lab 3</a></li>
+      <li className="nav-item"><a id="wd-k" href="#/Kanbas" className="nav-link">Kanbas</a></li>
+      <li className="nav-item"><a id="wd-github"
+        href="https://github.com/SizhuQu/kanbas-react-web-app.git" target="_blank"
+        className="nav-link">My GitHub</a></li>
     </ul>
   );
 }
