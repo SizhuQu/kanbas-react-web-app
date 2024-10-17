2330425f42dad5095a401b2b112c50bb08093636
Sizhu Qu <144123630+SizhuQu@users.noreply.github.com>
2024-10-16 21:25:06 -0400

    2.5

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -25,10 +25,13 @@ import Spreading from "./Spreading";
 import Destructing from "./Destructing";
 import FunctionDestructing from "./FunctionDestructing";
 import DestructingImports from "./DestructingImports";
+import Classes from "./Classes";
+import Styles from "./Styles";
+import Add from "./Add";
 
 export default function Lab3() {
   return (
-    <div id="wd-lab3">
+    <div id="wd-lab3" className="container">
       <h3>Lab 3</h3>
       <VariablesAndConstants />
       <VariableTypes />
@@ -57,6 +60,9 @@ export default function Lab3() {
       <Destructing />
       <FunctionDestructing />
       <DestructingImports />
+      <Classes />
+      <Styles />
+      <Add a={3} b={4} />
     </div>
   );
 }
