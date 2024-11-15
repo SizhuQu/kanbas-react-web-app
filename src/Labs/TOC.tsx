2e4a2a3496c789b9f40ea483ead3bc234ea1c384
Sizhu Qu <144123630+SizhuQu@users.noreply.github.com>
2024-11-14 22:30:43 -0500

    3.1

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -10,13 +10,20 @@ export default function TOC() {
         </Link>
       </li>
       <li className="nav-item"><a id="wd-a1" href="#/Labs/Lab1"
-        className={`nav-link ${pathname.includes("Lab1") ? "active" : ""}`}>Lab 1</a></li>
+        className={`nav-link ${pathname.includes("Lab1") ? "active" : ""}`}>Lab 1</a>
+      </li>
       <li className="nav-item"><a id="wd-a2" href="#/Labs/Lab2"
-        className={`nav-link ${pathname.includes("Lab2") ? "active" : ""}`}>Lab 2</a></li>
+        className={`nav-link ${pathname.includes("Lab2") ? "active" : ""}`}>Lab 2</a>
+      </li>
       <li className="nav-item"><a id="wd-a3" href="#/Labs/Lab3"
-        className={`nav-link ${pathname.includes("Lab3") ? "active" : ""}`}>Lab 3</a></li>
+        className={`nav-link ${pathname.includes("Lab3") ? "active" : ""}`}>Lab 3</a>
+      </li>
       <li className="nav-item"><a id="wd-a4" href="#/Labs/Lab4"
-        className={`nav-link ${pathname.includes("Lab4") ? "active" : ""}`}>Lab 4</a></li>
+        className={`nav-link ${pathname.includes("Lab4") ? "active" : ""}`}>Lab 4</a>
+      </li>
+      <li className="nav-item"><a id="wd-a5" href="#/Labs/Lab5"
+        className={`nav-link ${pathname.includes("Lab5") ? "active" : ""}`}>Lab 5</a>
+      </li>
       <li className="nav-item"><a id="wd-k" href="#/Kanbas"
         className="nav-link">Kanbas</a></li>
       <li className="nav-item">
