cf561554557b138c176f554d19b4b5ce75195c03
Sizhu Qu <144123630+SizhuQu@users.noreply.github.com>
2024-11-23 01:04:48 -0500

    3.6.3

--- src/Labs/Lab5/WorkingWithArraysAsynchronously.tsx
+++ src/Labs/Lab5/WorkingWithArraysAsynchronously.tsx
@@ -1,9 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { FaTrash } from "react-icons/fa";
 import { FaPlusCircle } from "react-icons/fa";
+import { FaPencil } from "react-icons/fa6";
+import { TiDelete } from "react-icons/ti";
 import * as client from "./client";
 export default function WorkingWithArraysAsynchronously() {
     const [todos, setTodos] = useState<any[]>([]);
+    const editTodo = (todo: any) => {
+        const updatedTodos = todos.map(
+            (t) => t.id === todo.id ? { ...todo, editing: true } : t);
+        setTodos(updatedTodos);
+    };
+    const updateTodo = async (todo: any) => {
+        await client.updateTodo(todo);
+        setTodos(todos.map((t) => (t.id === todo.id ? todo : t)));
+    };
+
     const fetchTodos = async () => {
         const todos = await client.fetchTodos();
         setTodos(todos);
@@ -18,6 +30,19 @@ export default function WorkingWithArraysAsynchronously() {
         setTodos(todos);
     };
 
+    const postTodo = async () => {
+        const newTodo = await client.postTodo({
+            title: "New Posted Todo",
+            completed: false,
+        });
+        setTodos([...todos, newTodo]);
+    };
+
+    const deleteTodo = async (todo: any) => {
+        await client.deleteTodo(todo);
+        const newTodos = todos.filter((t) => t.id !== todo.id);
+        setTodos(newTodos);
+    };
 
     useEffect(() => {
         fetchTodos();
@@ -30,17 +55,54 @@ export default function WorkingWithArraysAsynchronously() {
                 onClick={createTodo}
                 className="text-success float-end fs-3"
                 id="wd-create-todo" />
-            <ul className="list-group">
-                {todos.map((todo) => (
-                    <li key={todo.id} className="list-group-item">
-                        <FaTrash onClick={() => removeTodo(todo)}
-                            className="text-danger float-end mt-1" id="wd-remove-todo" />
+            <FaPlusCircle
+                onClick={postTodo}
+                className="text-primary float-end fs-3 me-3"
+                id="wd-post-todo" />
 
-                        <input type="checkbox" className="form-check-input me-2"
-                            defaultChecked={todo.completed} />
-                        <span style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
-                            {todo.title}
-                        </span>
+            <ul
+                className="list-group">
+                {todos.map((todo) => (
+                    <li
+                        key={todo.id}
+                        className="list-group-item">
+
+                        <FaTrash
+                            onClick={() => removeTodo(todo)}
+                            className="text-danger float-end mt-1"
+                            id="wd-remove-todo" />
+                        <TiDelete
+                            onClick={() => deleteTodo(todo)}
+                            className="text-danger float-end me-2 fs-3"
+                            id="wd-delete-todo" />
+                        <FaPencil
+                            onClick={() => editTodo(todo)}
+                            className="text-primary float-end me-2 mt-1" />
+
+                        <input
+                            type="checkbox"
+                            className="form-check-input me-2"
+                            defaultChecked={todo.completed}
+                            onChange={(e) => updateTodo({ ...todo, completed: e.target.checked })} />
+                        {!todo.editing ? (
+                            <span
+                                style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
+                                {todo.title}
+                            </span>
+                        ) : (
+                            <input
+                                className="form-control w-50 float-start"
+                                defaultValue={todo.title}
+                                onKeyDown={(e) => {
+                                    if (e.key === "Enter") {
+                                        updateTodo({ ...todo, editing: false });
+                                    }
+                                }}
+                                onChange={(e) =>
+                                    updateTodo({ ...todo, title: e.target.value })
+                                }
+                            />
+                        )}
                     </li>
                 ))}
             </ul> <hr />
