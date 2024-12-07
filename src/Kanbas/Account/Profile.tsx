c5445e15688f3fdf074dfb630e871daeefcf9d39
Sizhu Qu <144123630+SizhuQu@users.noreply.github.com>
2024-12-07 02:54:26 -0500

    change

--- src/Kanbas/Account/Profile.tsx
+++ src/Kanbas/Account/Profile.tsx
@@ -2,13 +2,15 @@ import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { setCurrentUser } from "./reducer";
-import React, { useCallback } from "react";
+import { useCallback } from "react";
 import * as client from "./client";
+
 export default function Profile() {
   const [profile, setProfile] = useState<any>({});
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { currentUser } = useSelector((state: any) => state.accountReducer);
+
   const updateProfile = async () => {
     const updatedProfile = await client.updateUser(profile);
     dispatch(setCurrentUser(updatedProfile));
@@ -21,12 +23,14 @@ export default function Profile() {
       setProfile(currentUser);
     }
   }, [currentUser, navigate]);
+
   const signout = async () => {
     await client.signout();
     dispatch(setCurrentUser(null));
     navigate("/Kanbas/Account/Signin");
   };
-  useEffect(() => { fetchProfile(); }, []);
+
+  useEffect(() => { fetchProfile(); }, [fetchProfile]);
   return (
     <div className="wd-profile-screen">
       <h3>Profile</h3>
@@ -55,7 +59,9 @@ export default function Profile() {
             <option value="FACULTY">Faculty</option>
             <option value="STUDENT">Student</option>
           </select>
-          <button onClick={updateProfile} className="btn btn-primary w-100 mb-2"> Update </button>
+          <button onClick={updateProfile} className="btn btn-primary w-100 mb-2">
+            Update
+          </button>
           <button onClick={signout} className="btn btn-danger w-100 mb-2" id="wd-signout-btn">
             Sign out
           </button>
